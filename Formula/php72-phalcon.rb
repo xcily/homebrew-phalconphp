f5815590c3f4bf75a5622f8471c958525f6ed17e
Ben Casey <bcasey@tigerstrikemedia.com>
2018-04-01 19:22:37 +0930

    Removing php55-phalcon as php55 no longer exists..
    Adding bottle for php72-phalcon

--- Formula/php72-phalcon.rb
+++ Formula/php72-phalcon.rb
@@ -8,6 +8,11 @@ class Php72Phalcon < AbstractPhp72Extension
   sha256 "823fd693a7e9e8999edfd405970a81b7bf731fa28109a64774216fc5f68d2975"
   head "https://github.com/phalcon/cphalcon.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "97a93ce644eac6e02bbe2f7080f629790848a204ea7b739e4ddb51258572363b" => :high_sierra
+  end
+
   depends_on "pcre"
 
   def install
