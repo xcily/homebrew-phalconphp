252e5530142e67dc2edc80f7189a2ed47f2a6cd3
Ben Casey <bcasey@tigerstrikemedia.com>
2018-06-11 17:46:45 +0930

    Remove old bottle and SHA information

--- Formula/php71-phalcon.rb
+++ Formula/php71-phalcon.rb
@@ -5,14 +5,6 @@ class Php71Phalcon < AbstractPhp71Extension
   desc "Full-stack PHP framework"
   homepage "https://phalconphp.com/"
   url "https://github.com/phalcon/cphalcon/archive/v3.4.0.zip"
-  sha256 "acbffa40a928672d1874345c2aa4eee6f05e087c943e155031db92db3cdc7429"
-  head "https://github.com/phalcon/cphalcon.git"
-
-  bottle do
-    cellar :any_skip_relocation
-    rebuild 1
-    sha256 "45b3f37b1d51aada03338e29881b37ce823d773cd9010b355f78f67f1e8bd80e" => :high_sierra
-  end
 
   depends_on "pcre"
 
