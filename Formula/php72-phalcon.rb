58763045f8b83fa07af3a587c7720f30c13d514c
xcily <xcily1@163.com>
2018-10-18 17:32:39 +0800

    v3.4.1

--- Formula/php72-phalcon.rb
+++ Formula/php72-phalcon.rb
@@ -4,8 +4,7 @@ class Php72Phalcon < AbstractPhp72Extension
   init
   desc "Full-stack PHP framework"
   homepage "https://phalconphp.com/"
-  url "https://github.com/phalcon/cphalcon/archive/v3.4.0.zip"
-  sha256 "acbffa40a928672d1874345c2aa4eee6f05e087c943e155031db92db3cdc7429"
+  url "https://github.com/phalcon/cphalcon/archive/v3.4.1.zip"
   head "https://github.com/phalcon/cphalcon.git"
 
   depends_on "pcre"
